ab550bcd349707a14956f23f27f5cc4bc4bb78bf
Oumar Ndiaye <96336953+dev-fun7@users.noreply.github.com>
2025-06-19 09:39:36 +0200

    retrieving from thesis project

---
+++ scripts/lib/extra-utils.R
@@ -0,0 +1,6 @@
+#' @param libraries a vector of library names to install
+#' @param pkgManager the package manager's repository
+
+intall_libraries <- function(libraries, pkgManager) {
+  # to-do
+}
